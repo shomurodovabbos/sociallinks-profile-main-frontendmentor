c92b1c5e23eab791fa9a59b77c32a57d9b02b817
abbos91 <abbos.shomurodov91@gmail.com>
2026-05-11 23:21:14 +0500

    htmlfile updated

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+    content: ["./index.html"],
+    theme: {
+        extend: {
+            colors: {
+                green: "hsl(75,94%,57%)",
+                white: "hsl(0,0%,100%)",
+                grey700: "hsl(0,0%,20%)",
+                grey800: "hsl(0,0%,12%)",
+                grey900: "hsl(0,0%,8%)",
+            },
+            fontFamily: {
+                sans: ["Inter", "sans-serif"],
+            },
+        },
+    },
+};
